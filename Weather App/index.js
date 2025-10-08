f194ddb67ed600741ac68867b1228005a1d1689b
Kanimozhi-V <kanimozhi.v@10decoders.in>
2025-10-08 19:21:56 +0530

    change in Apikey

--- Weather App/index.js
+++ Weather App/index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector('.weatherForm');
 const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
-const apiKey = "9066b40f066bbacfc58cfaea56f8efa0";
+const apiKey = "******************************";
 
 weatherForm.addEventListener('submit', async event => {
     event.preventDefault();
